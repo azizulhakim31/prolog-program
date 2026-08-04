88bd39f3350b3227360ac7f2507ef239b2f82b6b
Md. Azizul Hakim <aazizul924@gmail.com>
2026-08-04 23:16:09 +0600

    prolog-1

---
+++ addition_and_multiplication_of_two_numbers.pl
@@ -0,0 +1,15 @@
+start :-
+write('Enter first number: '),
+read(X),
+write('Enter second number: '),
+read(Y),
+add(X, Y, Sum),
+multiply(X, Y, Product),
+write('Sum = '),
+write(Sum), nl,
+write('Product = '),
+write(Product), nl.
+add(X, Y, Sum) :-
+Sum is X + Y.
+multiply(X, Y, Product) :-
+Product is X * Y.
